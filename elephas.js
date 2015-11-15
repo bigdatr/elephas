07be111eac36bdc2e452a0e06943af17b70cabd3
Atridge D'Costa <atridge.dcosta@blueflag.com.au>
2015-11-16 10:31:26 +1100

    added tests

--- elephas.js
+++ elephas.js
@@ -1,1 +1,2 @@
+require('babel-core/register');
 module.exports = require('./lib/framework');
---
+++ test/wsRouter-test.js
@@ -0,0 +1,30 @@
+import {expect} from 'chai';
+import sinon from 'sinon';
+import wsRouter from '../lib/wsRouter';
+
+const _testRoutes = [{
+    MY_TEST_ACTION: sinon.spy()
+}];
+
+describe('wsRouter', () => {
+    it('has no routes by default', () => {
+        expect(wsRouter.hasRoutes()).to.equal(false);
+    });
+
+    it('can add websocket routes/action handlers', () => {
+        wsRouter.setActionHandlers(_testRoutes);
+
+        expect(wsRouter.hasRoutes()).to.equal(true);
+    });
+
+    it('can route to the correct action', () => {
+        const client = {};
+        const action = {type: 'MY_TEST_ACTION', payload: 'test'};
+
+        wsRouter.route(client, action);
+
+        const fn = _testRoutes[0].MY_TEST_ACTION;
+
+        expect(fn.called).to.equal(true);
+    });
+});
